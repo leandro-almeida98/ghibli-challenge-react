10926643824fba7556b87865717f7c9da18ff6b5
Leandro Sacramento de Almeida <leandro.almeida98@hotmail.com>
2026-02-20 18:55:00 -0300

    chore: configurar Tailwind CSS, estrutura de pastas e ponto de entrada da aplicacao

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
+import { queryClient } from './lib/queryClient'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster richColors position="bottom-right" />
+    </QueryClientProvider>
   </StrictMode>,
 )
+
